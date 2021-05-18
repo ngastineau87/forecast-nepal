eba5c328ea5f15a9da66ea8b181051f09751a067
Gastineau <ngastineau87@gmail.com>
2021-05-18 09:47:25 +0200

    update readme and add comments

--- src/Mapchart.js
+++ src/Mapchart.js
@@ -5,8 +5,8 @@ import { Link, BrowserRouter } from "react-router-dom";
 import data from './cities.json'; // import list of the cities
 import forecastdata from './forecast.json'; // import list of the forecast for each city
 
-import { WiDaySunny,WiDaySunnyOvercast,WiCloud,WiFog,WiRainMix,WiShowers,WiSnow,WiSleet,WiThunderstorm,WiWindy} from 'weather-icons-react';
-import Highcharts from 'highcharts';
+import { WiDaySunny,WiDaySunnyOvercast,WiCloud,WiFog,WiRainMix,WiShowers,WiSnow,WiSleet,WiThunderstorm,WiWindy} from 'weather-icons-react';//icons to describe the global weather of a city
+import Highcharts from 'highcharts';//graphic to describe the temperature and humidity of a city
 import HighchartsReact from 'highcharts-react-official';
 
 import {
@@ -21,13 +21,10 @@ import {
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-var i;
-var mark =[];
+const fdata = forecastdata['data'];//data about the weather in different city
 
-const fdata = forecastdata['data'];
-
-function createoption(c_title,c_serie,t_or_h){//function to create parameter in highchart
-    if(t_or_h==='t'){
+function createoption(c_title,c_serie,t_or_h){//function to create parameter in highcharts
+    if(t_or_h==='t'){//the option of the highchart in the case we want a temperature
         return(
         {
         chart: {
@@ -73,7 +70,7 @@ function createoption(c_title,c_serie,t_or_h){//function to create parameter in 
         }
         )
     }
-    else{
+    else{//the option of the highchart in the case we want humidity
         return(
             {
             chart: {
@@ -122,16 +119,19 @@ function createoption(c_title,c_serie,t_or_h){//function to create parameter in 
     }
   }
 
+var i;
+var mark =[];//this table will be used to create marker on the map
+  
 for (i = 4956; i <= 5032; i++) {//this loop is done to create a list of marks containing data from cities.json to create the marker on the map
   var des = data[i].description;
   var lon = data[i].longitude;
   var la = data[i].latitude;
-  var idd = data[i].id;
+  var idd = data[i].id;//id of the area
   var spec_data = fdata[i];
   var spec_data2 = spec_data['params'];
   var spec_data3 = spec_data2['ww'];
   var spec_data4 = spec_data3['202105171200'];
-  var gbweather = spec_data4['code'];
+  var gbweather = spec_data4['code'];//this variable contain the code of the weather of the 2021 17 may 12H
   mark.push({id: idd, name: des, coordinates: [lon,la], gbw: gbweather });
 }
 
@@ -144,22 +144,22 @@ class MapChart extends  React.Component {
   constructor(props) {
     super(props);
       this.state = {
-        clickedcity: "",
-        clickedid: 0,
-        clickedparam: 't',
+        clickedcity: "",//selected city
+        clickedid: 0,//id of the selected city
+        clickedparam: 't',// temperature or humidity
       };
   };
-  createhighchart(){
+  createhighchart(){//function used to create highchart
     var id = this.state.clickedid;
     var param = this.state.clickedparam;
-    if(id){
+    if(id){//true if a city is selected
         var spec_data = fdata[id];
         var spec_data2 = spec_data['params'];
         var tabdata = [];
         if(param==='t'){
             var temp_data = spec_data2['t'];
             for(i in temp_data){
-                if(i!=='description'){
+                if(i!=='description'){//only select the data
                     var celtemp = temp_data[i];
                     tabdata.push(parseFloat(celtemp['C']));
                 }
@@ -174,7 +174,7 @@ class MapChart extends  React.Component {
         else{
             var hu_data = spec_data2['hu'];
             for(i in hu_data){
-                if(i!=='description'){
+                if(i!=='description'){//only select the data
                     var hutemp = hu_data[i];
                     tabdata.push(parseFloat(hutemp['%']));
                 }
@@ -188,7 +188,7 @@ class MapChart extends  React.Component {
         }
     }
   }
-  createbuttons(){
+  createbuttons(){//create the two button to change the data of the highchart
     var id = this.state.clickedid;
     if(id){
         return(
@@ -200,7 +200,7 @@ class MapChart extends  React.Component {
     }
  
   }
-  createweathericon(code){
+  createweathericon(code){//transform weather codes into icons
         if(code==='1' ||code==='2'){
             return(
             <WiDaySunny size={20} color='#000' />
